7ff3cdad3796ffd24bf01e8234205748dc8961e5
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2020-11-06 19:13:59 -0600

    added working laravel project

---
+++ First_Exercise/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tarea', function() {
+    //return 'esta es la pagina de tareas';
+
+    return redirect()->route('catX');
+});
+
+Route::get('/tarea/{id}/{nombre}', function($id, $nombre) {
+    return 'Pagina de la tarea' . $id . 'con el nombre' . $nombre;
+});
+
+Route::get('/menu1/submenu3/categoriaX', function() {
+    return 'pagina de prueba';
+})->name('catX');
+
